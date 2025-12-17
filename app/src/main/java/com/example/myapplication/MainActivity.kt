9701d7528d8e8cfd64d30fa9c8ea9302399d51de
Annapoorna <annapoornakamathc@gmail.com>
2025-12-17 18:36:03 +0530

    * States

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -32,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,7 +51,12 @@ class MainActivity : ComponentActivity() {
             Font(R.font.lexend_semibold, FontWeight.SemiBold)
         )
         setContent {
-            Column {
+            Column(
+                modifier = Modifier.verticalScroll(
+                    enabled = true,
+                    state = rememberScrollState()
+                )
+            ) {
                 AnnotatedText(fontFamily)
                 ImageCard(
                     painter = painterResource(R.drawable.bird),
@@ -51,6 +64,7 @@ class MainActivity : ComponentActivity() {
                     title = "This is a Kingfisher bird.",
                     fontFamily = fontFamily
                 )
+                ColorBox(modifier = Modifier)
             }
         }
     }
@@ -64,7 +78,11 @@ fun ImageCard(
     modifier: Modifier = Modifier,
     fontFamily: FontFamily
 ) {
-    Box(modifier = Modifier.fillMaxSize(0.5f).padding(12.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(0.5f)
+            .padding(12.dp)
+    ) {
         Card(
             modifier = modifier.fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
@@ -80,7 +98,8 @@ fun ImageCard(
                 )
 
                 Box(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .background(
                             brush = Brush.verticalGradient(
                                 colors = listOf(
@@ -131,5 +150,23 @@ fun AnnotatedText(fontFamily: FontFamily) {
         style = MaterialTheme.typography.bodyLarge,
         fontFamily = fontFamily
     )
+}
 
+@Composable
+fun ColorBox(modifier: Modifier = Modifier) {
+    var color by remember { mutableStateOf(Color.Yellow) }
+
+    Box(modifier = modifier
+        .padding(40.dp)
+        .background(color = color)
+        .clickable {
+            color = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+        }) {
+        Text(text = "Click to change color", modifier = modifier.padding(16.dp))
+    }
 }
