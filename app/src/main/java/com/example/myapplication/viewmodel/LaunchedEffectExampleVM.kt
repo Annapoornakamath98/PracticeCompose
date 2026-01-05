4361bddcb500f7bb78eb0cda4fdd9b330c37209e
Annapoorna <annapoornakamathc@gmail.com>
2026-01-05 18:30:46 +0530

    * Ktlint integration

--- app/src/main/java/com/example/myapplication/viewmodel/LaunchedEffectExampleVM.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/LaunchedEffectExampleVM.kt
@@ -17,7 +17,7 @@ class LaunchedEffectExampleVM
 
         init {
             viewModelScope.launch {
-                _sharedFlow.emit(ScreenEvents.ShowSnackBar("Hello!"))
+                _sharedFlow.emit(ScreenEvents.ShowSnackBar("Hello world!"))
             }
         }
     }
